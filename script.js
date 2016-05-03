0a44b0f26d8693c63e01382a473cd7f1468ae75f
Yusuf Matcheswala <ymatcheswala@gmail.com>
2016-05-02 21:47:27 -0400

    Initial commit of files for threadly web project

---
+++ script.js
@@ -0,0 +1,10 @@
+//This script will take a new entry from the input and modify the document
+//to display as a new list item.
+var main = function() {
+	$('form').submit(function(event){
+		
+	});
+
+}
+
+$(document).ready(main);
